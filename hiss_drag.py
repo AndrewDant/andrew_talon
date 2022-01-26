56f18b0156bf0cb6d6898b8463b2e853ed3d6b93
andrew.dant <andrew.dant@ert.com>
2022-01-26 11:09:01 -0500

    add minimum hiss time before starting drag

--- hiss_drag.py
+++ hiss_drag.py
@@ -1,22 +1,33 @@
+from talon import Module, actions, noise, cron
 from talon_plugins import eye_mouse
 
+min_hiss_time = "200ms"
+ongoing_hiss = False
 
 mod = Module()
-
 
 @mod.action_class
 class UserActions:
     def noise_hiss_start():
         """Invoked when the user starts hissing (potentially while speaking)"""
         print("hiss start")
-        # ctrl.mouse_click(0, down: true)
-        actions.mouse_drag(0)
+        global ongoing_hiss
+        ongoing_hiss = True
+
+        cron.after(min_hiss_time, validate_hiss)
 
     def noise_hiss_stop():
         """Invoked when the user finishes hissing (potentially while speaking)"""
-        
         print("hiss stop")
+        global ongoing_hiss
+        ongoing_hiss = False
+
         actions.mouse_release(0)
+
+
+def validate_hiss():
+    if ongoing_hiss:
+        actions.mouse_drag(0)
 
 
 def hiss_handler(active):
