1f2dbeec38df94366ebbd22ca491077186761e23
Andrew Dant <andrewdant17@gmail.com>
2022-09-12 20:11:12 -0400

    hiss to right click and obelisk settings

---
+++ gaming/AcrossTheObelisk.talon
@@ -0,0 +1,9 @@
+app: AcrossTheObelisk.exe
+-
+settings():
+    user.mouse_enable_pop_click = 0
+    user.mouse_enable_pop_drag = 1
+    # key_wait = 64
+    
+# key(shift-f10:down): mouse_drag(0)
+# key(shift-f10:up): mouse_release(0)
---
+++ hiss_click.py
@@ -0,0 +1,34 @@
+from talon import Module, actions, noise
+from talon_plugins import eye_mouse
+
+ongoing_hiss = False
+
+mod = Module()
+
+@mod.action_class
+class UserActions:
+    def noise_hiss_start():
+        """Invoked when the user starts hissing (potentially while speaking)"""
+        print("hiss start")
+
+        actions.mouse_click(1)
+
+    def noise_hiss_stop():
+        """Invoked when the user finishes hissing (potentially while speaking)"""
+        print("hiss stop")
+
+
+def hiss_handler(active):
+    print('hiss handler: ' + str(active))
+    # aegis says this api will definitely change
+    active_eyetracker = eye_mouse.mouse.attached_tracker is not None
+
+    if active_eyetracker:
+        if active:
+            actions.user.noise_hiss_start()
+        else:
+            actions.user.noise_hiss_stop()
+
+
+# register the handler to the noise
+noise.register("hiss", hiss_handler)
