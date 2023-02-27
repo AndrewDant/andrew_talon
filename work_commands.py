53617a682f4dd35357d753db71a3897072a10cf5
Andrew Dant <andrewdant17@gmail.com>
2023-02-27 13:17:35 -0500

    improve the jira link command

--- work_commands.py
+++ work_commands.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 from talon import Module, actions, app
 
@@ -21,17 +22,27 @@ class WorkCommands:
         if not selected:
             print("Asked to open link to selection, but nothing selected.")
             return
+            
+        try:
+            ticket_id = re.search('(ONEECOA-\d+)', selected, flags=re.IGNORECASE).group(1)
+        except AttributeError:
+            print("Could not extract jira ticket id")
+            return
         
-        running_app = actions.user.get_running_app("Microsoft Edge")
+        running_app_list = [app for app in actions.user.get_running_app_list("Microsoft Edge") if app.active_window and app.active_window.title]
+        # TODO maybe a better way to filter this in the future
+        running_app = running_app_list[0]
         if running_app and running_app.active_window and running_app.active_window.title:
             actions.user.switcher_focus_app(running_app)
+            actions.sleep("250ms")
         else:
             # TODO I'd like to find a better way to reference it
             actions.user.switcher_launch("Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge")
+            actions.sleep("500ms")
         
-        actions.sleep("100ms")
         actions.app.tab_open()
         actions.browser.focus_address()
-        actions.insert(f"https://jira.ert.com/jira/browse/{selected}")
+        actions.sleep("100ms")
+        actions.insert(f"https://jira.ert.com/jira/browse/{ticket_id}")
         actions.sleep("100ms")
         actions.key("enter")
