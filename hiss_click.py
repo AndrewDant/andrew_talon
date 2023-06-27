c4a117ba0ec49366196b0254ab24801602c3fb70
Andrew Dant <andrew.dant@clario.com>
2023-06-27 17:06:08 -0400

    hiss click without laser

--- hiss_click.py
+++ hiss_click.py
@@ -5,29 +5,27 @@ ongoing_hiss = False
 
 mod = Module()
 
+
 @mod.action_class
 class UserActions:
     def noise_hiss_start():
         """Invoked when the user starts hissing (potentially while speaking)"""
-        print("hiss start")
 
         ctrl.mouse_click(button=1, hold=16000)
 
     def noise_hiss_stop():
         """Invoked when the user finishes hissing (potentially while speaking)"""
-        print("hiss stop")
 
 
 def hiss_handler(active):
-    print('hiss handler: ' + str(active))
     # aegis says this api will definitely change
     active_eyetracker = eye_mouse.mouse.attached_tracker is not None
 
-    if active_eyetracker:
-        if active:
-            actions.user.noise_hiss_start()
-        else:
-            actions.user.noise_hiss_stop()
+    # if active_eyetracker:
+    if active:
+        actions.user.noise_hiss_start()
+    else:
+        actions.user.noise_hiss_stop()
 
 
 # register the handler to the noise
