bbde111faa333c0f0a67c32d9ae7ddd708b8ff6f
Andrew Dant <andrew.dant@clario.com>
2023-10-04 22:57:03 -0400

    revert hiss from right click to drag

--- hiss_drag.py
+++ hiss_drag.py
@@ -1,27 +1,39 @@
-from talon import Module, actions, noise, ctrl
+from talon import Module, actions, noise, cron, scope
 from talon_plugins import eye_mouse
 
+min_hiss_time = "200ms"
 ongoing_hiss = False
 
 mod = Module()
-
 
 @mod.action_class
 class UserActions:
     def noise_hiss_start():
         """Invoked when the user starts hissing (potentially while speaking)"""
+        print("hiss start")
+        global ongoing_hiss
+        ongoing_hiss = True
 
-        ctrl.mouse_click(button=1, hold=16000)
+        # TODO this may misfire if there are a series of s sounds close together
+        cron.after(min_hiss_time, validate_hiss)
 
     def noise_hiss_stop():
         """Invoked when the user finishes hissing (potentially while speaking)"""
+        print("hiss stop")
+        global ongoing_hiss
+        ongoing_hiss = False
+
+        actions.mouse_release(0)
+
+
+def validate_hiss():
+    if ongoing_hiss:
+        actions.mouse_drag(0)
 
 
 def hiss_handler(active):
-    # aegis says this api will definitely change
-    active_eyetracker = eye_mouse.mouse.attached_tracker is not None
+    print('hiss handler: ' + str(active))
 
-    # if active_eyetracker:
     if active:
         actions.user.noise_hiss_start()
     else:
