5c306876a9359aae4e682600e5cd8574170c6eb5
Andrew Dant <andrew.dant@clario.com>
2023-10-10 17:22:36 -0400

    significant improvement to hiis drag

--- hiss_drag.py
+++ hiss_drag.py
@@ -3,7 +3,8 @@ from talon_plugins import eye_mouse
 from datetime import datetime, timedelta
 
 latest_hiss_start = None
-min_hiss_milliseconds = 1000
+min_hiss_milliseconds = 800
+hiss_cron = None
 
 mod = Module()
 
@@ -11,28 +12,33 @@ mod = Module()
 class UserActions:
     def noise_hiss_start():
         """Invoked when the user starts hissing (potentially while speaking)"""
-        global latest_hiss_start
+        global latest_hiss_start, hiss_cron
         
         if not latest_hiss_start:
             latest_hiss_start = datetime.now()
-            cron.after(f"{min_hiss_milliseconds + 5}ms", validate_hiss)
+            hiss_cron = cron.after(f"{min_hiss_milliseconds + 5}ms", drag_handler)
 
     def noise_hiss_stop():
         """Invoked when the user finishes hissing (potentially while speaking)"""
-        global latest_hiss_start
-        # probably need a better check here
-        if latest_hiss_start:
+        global latest_hiss_start, hiss_cron
+        if ongoing_hiss():
             actions.mouse_release(0)
         
         latest_hiss_start = None
+        cron.cancel(hiss_cron)
 
 
-def validate_hiss():
-    if latest_hiss_start and datetime.now() - latest_hiss_start >= timedelta(milliseconds=min_hiss_milliseconds):
+def drag_handler():
+    if ongoing_hiss():
         actions.mouse_drag(0)
 
 
+def ongoing_hiss():
+    return latest_hiss_start and datetime.now() - latest_hiss_start >= timedelta(milliseconds=min_hiss_milliseconds)
+
+
 def hiss_handler(active):
+    # NOTE: as of 10/10/23 literally just clicking my mouse triggered a hiss start/stop
     if active:
         actions.user.noise_hiss_start()
     else:
