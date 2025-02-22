e1487ac6da1f324cdcb109486e2cbd6da90d56e2
Andrew Dant <andrew.dant@clario.com>
2025-02-22 14:31:08 -0500

    tweak parrot scroll and zoom speed

--- parrot_actions.py
+++ parrot_actions.py
@@ -1,7 +1,11 @@
 from talon import Module, actions
 
 mod = Module()
+
 whistle_mode = 'scroll'
+
+scroll_multiplier = 0.5
+zoom_multiplier = scroll_multiplier
 
 @mod.action_class
 class UserActions:
@@ -9,20 +13,20 @@ class UserActions:
         """Transform the whistle volume into an amount to scroll downwards"""
         match whistle_mode:
             case 'scroll':
-                actions.mouse_scroll(-1 * power)
+                actions.mouse_scroll(-1 * power * scroll_multiplier)
             case 'zoom':
                 actions.key('ctrl:down')
-                actions.mouse_scroll(-1 * power)
+                actions.mouse_scroll(-1 * power * zoom_multiplier)
                 actions.key('ctrl:up')
     
     def whistle_down(power: float):
         """Transform the whistle volume into an amount to scroll upwards"""
         match whistle_mode:
             case 'scroll':
-                actions.mouse_scroll(power)
+                actions.mouse_scroll(power * scroll_multiplier)
             case 'zoom':
                 actions.key('ctrl:down')
-                actions.mouse_scroll(power)
+                actions.mouse_scroll(power * zoom_multiplier)
                 actions.key('ctrl:up')
 
     def set_whistle_mode(mode: str):
