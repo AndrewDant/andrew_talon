f990633f5e613b0179408db27a913d0afafb2e97
Andrew Dant <andrew.dant@clario.com>
2025-06-17 13:10:47 -0400

    devtools tweaks

--- apps/DevTools.py
+++ apps/DevTools.py
@@ -2,9 +2,12 @@ from talon import Context, actions
 
 ctx = Context()
 
+ctx.matches = r"""
+title: /DevTools/
+"""
+
 @ctx.action_class("edit")
 class EditActions:
-    # talon edit actions
     def jump_line(n: int):
         actions.key("ctrl-p")
         actions.sleep("100ms")
